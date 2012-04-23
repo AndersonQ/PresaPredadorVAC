f8f7800e2c8f47f32d9f4fbea5b158ce1d0e6d63
Tiago de França Queiroz <contato@tiago.eti.br>
2012-04-23 11:30:58 -0300

    Arrumei o métido ContaVizinhosR1, criei o métido ProcessaPresas e posValida

--- Simulador.java
+++ Simulador.java
@@ -18,6 +18,7 @@
  */
 
 import java.io.*;
+import java.util.*;
 
 public class Simulador
 {
@@ -27,14 +28,45 @@ public class Simulador
 	public static final int DEFUNTO = 3;
 	public static final int RECICLADOR = 4;
 
-	int ContaVizinhos(Celula[][] mapa, int tipo, int l, int c)
-	{
-		int i, j, cont = 0;
+	protected Celula[][] mapa_atual, mapa_prox;
+	protected int l, c;
+	Random r;
 
-		for(i = 0; i < l; i++)
-			for(j = 0; j < c; j++)
-				if(mapa[i][j].tipo == tipo)
-					cont++;
+	Simulador()
+	{
+		mapa_atual = mapa_prox = null;
+		l = c = 0;
+		r = new Random();
+	}
+
+	int ContaVizinhosR1(Celula[][] mapa, int tipo, int l, int c)
+	{
+		int cont = 0;
+
+		if(posValida(l - 1, c - 1))
+			if(mapa[l - 1][c - 1].tipo == tipo)
+				cont ++;
+		if(posValida(l - 1, c))
+			if(mapa[l - 1][c].tipo == tipo)
+				cont ++;
+		if(posValida(l - 1, c + 1))
+			if(mapa[l - 1][c + 1].tipo == tipo)
+				cont++;
+		if(posValida(l, c - 1))
+			if(mapa[l][c - 1].tipo == tipo)
+				cont ++;
+		if(posValida(l, c + 1))
+			if(mapa[l][c + 1].tipo == tipo)
+				cont ++;
+		if(posValida(l + 1, c - 1))
+			if(mapa[l + 1][c - 1].tipo == tipo)
+				cont ++;
+		if(posValida(l + 1, c))
+			if(mapa[l + 1][c].tipo == tipo)
+				cont ++;
+		if(posValida(l + 1, c + 1))
+			if(mapa[l + 1][c + 1].tipo == tipo)
+				cont ++;
 
 		return cont;
 	}
@@ -51,4 +83,54 @@ public class Simulador
 				m[i][j] = new Celula(mapa[i][j], 20);
 		return m;
 	}
+
+	/*
+	 * Morre: 4 ou mais vizinhos são presas (morre de fome)
+	 * Morre: Predadores > presas
+	 * Anda : vPresas >= vPredadores 
+	 */
+	void processaPresas()
+	{
+		int i, j;
+		int vPresa, vPredador, x, y;
+
+		for(i = 0; i < l; i++)
+			for(j = 0; j < c; j++)
+			{
+				if(mapa_atual[i][j].tipo == PRESA)
+				{
+					vPresa = ContaVizinhosR1(mapa_atual, PRESA, i, j);
+					vPredador = ContaVizinhosR1(mapa_atual, PREDADOR, i, j);
+
+					/* Morre */
+					if((vPresa >= 4) || (vPredador > vPresa))
+						mapa_prox[i][j] = new Celula(DEFUNTO, 0);
+
+					/* Anda */
+					if(vPredador >= vPresa)
+					{
+						x = r.nextInt(2);
+						y = r.nextInt(2);
+
+						/* Calcula o sinal */
+						if(r.nextBoolean())
+							x = -x;
+						if(r.nextBoolean())
+							y = -y;
+
+						if(posValida(i + x, j + y))
+							if(mapa_prox[i + x][j + y].tipo == NADA)
+								mapa_prox[i + x][j + y] = new Celula(PRESA, mapa_prox[i][j].vida);
+					}
+				}
+			}
+	}
+
+	boolean posValida(int lin, int col)
+	{
+		if( ((lin >= 0 ) && (col >= 0)) && ((lin < l) && (col < c)) )
+			return true;
+		return false;
+	}
+
 }
