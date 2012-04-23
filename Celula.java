406165d4e9f3c3c4b852463f1bf9e6e1c274e7fb
Tiago de França Queiroz <contato@tiago.eti.br>
2012-04-23 10:38:33 -0300

    Criei uma classe para ser a célula e armazenar o tipo e o tempo de vida

---
+++ Celula.java
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Celula.java
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  23-04-2012 10:34:37
+ *       Revision:  none
+ *       Compiler:  javac
+ *         Author:  Anderson de França Queiroz (A.F.Q.), contato(.AT,)andersonq dot eti dot br
+ *         	    Tiago de França Queiroz, contato(.AT,)tiago dot eti dot br
+ *
+ *        Company:  Universidade Federal do ABC - UFABC
+ *
+ * =====================================================================================
+ */
+
+import java.io.*;
+
+public class Celula
+{
+	int tipo;
+	int vida;
+
+	Celula(int t, int v)
+	{
+		tipo = t;
+		vida = v;
+	}
+}
